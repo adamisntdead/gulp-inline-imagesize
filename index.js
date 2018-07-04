804add2f38c2ee98020f19cf254e66e8bc83dfe4
Adam Kelly <adam.guitar12@gmail.com>
2018-07-04 08:16:49 +0100

    Cleaned Source

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const through = require('through2');
-const PluginError = require('plugin-error');
+const through = require('through2')
+const PluginError = require('plugin-error')
 const sizes = require('./sizes')
 
 module.exports = options => {
-	options = options || {};
+  options = options || {}
 
-	return through.obj(function (file, enc, cb) {
-		if (file.isNull()) {
-			cb(null, file);
-			return;
-		}
+  return through.obj(function(file, enc, cb) {
+    if (file.isNull()) {
+      cb(null, file)
+      return
+    }
 
-		if (file.isStream()) {
-			cb(new PluginError('gulp-inline-imagesize', 'Streaming not supported'));
-			return;
-		}
+    if (file.isStream()) {
+      cb(new PluginError('gulp-inline-imagesize', 'Streaming not supported'))
+      return
+    }
 
-		try {
-			file.contents = Buffer.from(sizes(file.path, file.contents.toString()));
-			this.push(file);
-		} catch (err) {
-			this.emit('error', new PluginError('gulp-inline-imagesize', err));
-		}
+    try {
+      file.contents = Buffer.from(sizes(file.path, file.contents.toString()))
+      this.push(file)
+    } catch (err) {
+      this.emit('error', new PluginError('gulp-inline-imagesize', err))
+    }
 
-		cb();
-	});
-};
+    cb()
+  })
+}
--- sizes.js
+++ sizes.js
@@ -14,7 +14,7 @@ const path = require('path')
 //     if (!dimensions) return
 
 //     const replacement = tag.replace(/^<img/, "<!--" + dimensions.width + "x" + dimensions.height + "-->\n" + "<img data-width data-height")
-  
+
 //     contents = contents.replace(tag, replacement)
 //   })
 
@@ -23,10 +23,11 @@ const path = require('path')
 
 module.exports = (filepath, contents) => {
   // Match image tags
-  const matches = contents.match(/<img[^\>]+src=['"](?!http:|https:|\/\/|data:image)([^"']+)["'][^\>]*>/gm)
+  const matches = contents.match(
+    /<img[^\>]+src=['"](?!http:|https:|\/\/|data:image)([^"']+)["'][^\>]*>/gm
+  )
   // Get the sources from the image
-  const sources = matches
-    .map(tag => tag.match(/src=['"]([^"']+)["']/m)[1])
+  const sources = matches.map(tag => tag.match(/src=['"]([^"']+)["']/m)[1])
   // Get the dimensions of each image
   const dimensions = sources
     .map(src => path.join(path.dirname(filepath), src))
@@ -34,7 +35,13 @@ module.exports = (filepath, contents) => {
 
   // Add a comment before each image with the dimensions
   matches
-    .map((tag, i) => [tag, tag.replace(/^<img/, `<!-- ${dimensions[i].width} x ${dimensions[i].height} -->\n<img`)])
+    .map((tag, i) => [
+      tag,
+      tag.replace(
+        /^<img/,
+        `<!-- ${dimensions[i].width} x ${dimensions[i].height} -->\n<img`
+      )
+    ])
     .forEach(replacement => {
       contents = contents.replace(...replacement)
     })
