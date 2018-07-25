eeb45a38e4f0160e43b156837f5e2e2124307540
Adam Kelly <adam.guitar12@gmail.com>
2018-07-25 15:39:15 +0100

    :art: Removed old source code comment

--- sizes.js
+++ sizes.js
@@ -2,25 +2,6 @@ const probe = require('probe-image-size')
 const fs = require('fs')
 const path = require('path')
 
-// module.exports = (path, contents) => {
-//   const matches = contents.match( /<img[^\>]+src=['"](?!http:|https:|\/\/|data:image)([^"']+)["'][^\>]*>/gm)
-//   matches.forEach(tag => {
-//     if (tag.match(/(height|width)=/)) return
-
-//     const src = tag.match(/src=['"]([^"']+)["']/m)[1]
-//     const imgpath = path.replace(/[^\/]+$/, '') + src
-//     const dims = probe.sync(fs.readFileSync(imgpath))
-
-//     if (!dimensions) return
-
-//     const replacement = tag.replace(/^<img/, "<!--" + dimensions.width + "x" + dimensions.height + "-->\n" + "<img data-width data-height")
-
-//     contents = contents.replace(tag, replacement)
-//   })
-
-//   return contents
-// }
-
 module.exports = (filepath, contents) => {
   // Match image tags
   const matches = contents.match(
